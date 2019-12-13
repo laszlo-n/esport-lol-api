b23ed569fe51f79d6aac063084c9b638e0083719
Nagy László Mihály <55386821+n-laszlo-m@users.noreply.github.com>
2019-12-13 03:45:43 +0100

    fix minor ux bug regarding error msgs

--- script.js
+++ script.js
@@ -3,74 +3,75 @@ function bginit() {
 }
 
 function riot() {
-	document.getElementById('api-key').reportValidity();
-	document.getElementById('search').reportValidity();
+	if (document.getElementById('api-key').reportValidity()
+		&& document.getElementById('search').reportValidity())
+	{
+		let sname = document.getElementById('search').value.trim();
+		let apikey = document.getElementById('api-key').value.trim();
 
-	let sname = document.getElementById('search').value.trim();
-	let apikey = document.getElementById('api-key').value.trim();
 
+		var request = new XMLHttpRequest();
+		request.open('GET', "https://cors-anywhere.herokuapp.com/https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + sname + '?api_key=' + apikey, true);
+		request.onloadend = function () {
+			if (request.status >= 200 && request.status < 400) {
+				var data = JSON.parse(this.response);
 
-	var request = new XMLHttpRequest();
-	request.open('GET', "https://cors-anywhere.herokuapp.com/https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + sname + '?api_key=' + apikey, true);
-	request.onloadend = function () {
-		if (request.status >= 200 && request.status < 400) {
-			var data = JSON.parse(this.response);
+				let namecard = document.getElementById('namecard');
+				namecard.innerHTML = "";
+				namecard.style.display = "none";
 
-			let namecard = document.getElementById('namecard');
-			namecard.innerHTML = "";
-			namecard.style.display = "none";
+				let tempOuter = document.createElement('div');
+				tempOuter.classList.add('media');
+				document.getElementById('namecard').appendChild(tempOuter);
 
-			let tempOuter = document.createElement('div');
-			tempOuter.classList.add('media');
-			document.getElementById('namecard').appendChild(tempOuter);
+				let tempInner = document.createElement('div');
+				tempInner.classList.add('media-left');
 
-			let tempInner = document.createElement('div');
-			tempInner.classList.add('media-left');
+				let pfp = document.createElement('img');
+				pfp.classList.add('profile-img');
+				pfp.src = "https://ddragon.leagueoflegends.com/cdn/9.21.1/img/profileicon/" + data["profileIconId"] + ".png";
 
-			let pfp = document.createElement('img');
-			pfp.classList.add('profile-img');
-			pfp.src = "https://ddragon.leagueoflegends.com/cdn/9.21.1/img/profileicon/" + data["profileIconId"] + ".png";
+				tempInner.appendChild(pfp);
+				tempOuter.appendChild(tempInner);
 
-			tempInner.appendChild(pfp);
-			tempOuter.appendChild(tempInner);
+				let tempInner2 = document.createElement('div');
+				tempInner2.classList.add('media-body');
 
-			let tempInner2 = document.createElement('div');
-			tempInner2.classList.add('media-body');
+				let summonerName = document.createElement('h2');
+				summonerName.classList.add('media-heading');
+				summonerName.innerText = data["name"];
 
-			let summonerName = document.createElement('h2');
-			summonerName.classList.add('media-heading');
-			summonerName.innerText = data["name"];
+				tempInner2.appendChild(summonerName);
 
-			tempInner2.appendChild(summonerName);
+				let summonnerLevel = document.createElement('div');
+				summonnerLevel.innerText = "Szint: " + data["summonerLevel"];
 
-			let summonnerLevel = document.createElement('div');
-			summonnerLevel.innerText = "Szint: " + data["summonerLevel"];
+				tempInner2.appendChild(summonnerLevel);
 
-			tempInner2.appendChild(summonnerLevel);
+				tempOuter.appendChild(tempInner2);
 
-			tempOuter.appendChild(tempInner2);
+				namecard.style.display = "block";
+			} else {
+				let namecard = document.getElementById('namecard');
+				namecard.innerHTML = "";
+				namecard.style.display = "none";
 
-			namecard.style.display = "block";
-		} else {
-			let namecard = document.getElementById('namecard');
-			namecard.innerHTML = "";
-			namecard.style.display = "none";
+				let tempOuter = document.createElement('div');
+				tempOuter.classList.add('media');
+				document.getElementById('namecard').appendChild(tempOuter);
 
-			let tempOuter = document.createElement('div');
-			tempOuter.classList.add('media');
-			document.getElementById('namecard').appendChild(tempOuter);
+				let tempInner2 = document.createElement('div');
+				tempInner2.classList.add('media-body');
 
-			let tempInner2 = document.createElement('div');
-			tempInner2.classList.add('media-body');
-
-			let msg = document.createElement('center');
-			msg.classList.add('media-heading');
-			msg.innerText = "Nem találtam ilyen nevű idézőt, próbáld újra.";
-			tempInner2.appendChild(msg);
-			document.getElementById('namecard').appendChild(tempInner2);
-			namecard.style.display = "block";
+				let msg = document.createElement('center');
+				msg.classList.add('media-heading');
+				msg.innerText = "Nem találtam ilyen nevű idézőt, próbáld újra.";
+				tempInner2.appendChild(msg);
+				document.getElementById('namecard').appendChild(tempInner2);
+				namecard.style.display = "block";
+			}
 		}
-	}
 
-	request.send();
+		request.send();
+	}
 }
